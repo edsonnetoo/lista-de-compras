5357d57a63dab3e5e19f9eb413d164ef5f2a0884
edsonnetoo <edson.creeper@gmail.com>
2024-11-24 21:52:21 -0300

    Aula 01 - Integrando JS pt2

--- scripts.js
+++ scripts.js
@@ -1,9 +1,45 @@
 const item = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
 
 botaoSalvarItem.addEventListener("click", adicionarItem);
 
 function adicionarItem(evento) {
     evento.preventDefault();
-    console.log("entrou");
+    
+    const itemDaLista = document.createElement("li");
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("item-lista-container");
+
+    const containerNomeDoItem = document.createElement("div");
+    const nomeDoItem = document.createElement("p");
+    nomeDoItem.innerText = item.value;
+    containerNomeDoItem.appendChild(nomeDoItem);
+
+    const containerBotoes = document.createElement("div");
+    // criando botão remover
+    const botaoRemover = document.createElement("button");
+    botaoRemover.classList.add("botao-acao");
+
+    const imagemRemover = document.createElement("img");
+    imagemRemover.src = "img/delete.svg";
+    imagemRemover.alt = "Remover";
+
+    // Criando botão editar
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("botao-acao");
+
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/edit.svg";
+    imagemEditar.alt = "Editar";
+
+    botaoRemover.appendChild(imagemRemover);
+    botaoEditar.appendChild(imagemEditar);
+    containerBotoes.appendChild(botaoRemover);
+    containerBotoes.appendChild(botaoEditar);
+
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
+    itemDaLista.appendChild(containerItemLista);
+    listaDeCompras.appendChild(itemDaLista);
 }
