cce98812cad6a5d9ebdfc61435e3a24600549158
edsonnetoo <edson.creeper@gmail.com>
2024-11-26 14:48:02 -0300

    Aula 03  - Gerando dados dinâmicos pt2

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const item = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
+const listaComprados = document.getElementById("lista-comprados");
 let contador = 0;
 
 botaoSalvarItem.addEventListener("click", adicionarItem);
@@ -29,11 +30,16 @@ function adicionarItem(evento) {
     checkboxLabel.addEventListener("click", function (evento) {
         const checkboxInput = evento.currentTarget.querySelector(".checkbox-input");
         const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-custom");
+        const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo");
 
         if (checkboxInput.checked) {
             checkboxCustomizado.classList.add("checked");
+            itemTitulo.style.textDecoration = "line-through";
+            listaComprados.appendChild(itemDaLista);
         } else {
             checkboxCustomizado.classList.remove("checked");
+            itemTitulo.style.textDecoration = "none";
+            listaDeCompras.appendChild(itemDaLista);
         }
     })
 
@@ -47,6 +53,7 @@ function adicionarItem(evento) {
     containerNomeDoItem.appendChild(containerCheckbox);
 
     const nomeDoItem = document.createElement("p");
+    nomeDoItem.id = "item-titulo";
     nomeDoItem.innerText = item.value;
     containerNomeDoItem.appendChild(nomeDoItem);
 
